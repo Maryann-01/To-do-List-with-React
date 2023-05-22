f50be1de8e97fc7c7cbbc8db4d76b940891bfdfc
Maryann-01 <amaraizummesoma@gmail.com>
2023-05-22 18:22:31 +0100

    added a slash while importing react icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
-import { FaEdit,FaTrash } from "react-icons/fa";
+import { FaEdit,FaTrash } from "react-icons/fa/";
 
 
 function App() {
